d54fd7c44f71c0eb13d9cdd1d10c16db450ef17b
Celcus <arandomguy1337@gmail.com>
2015-12-07 17:12:08 -0600

    Update main.py
    added more allowedUsers

--- usr/bin/python/main.py
+++ usr/bin/python/main.py
@@ -1,13 +1,19 @@
 import os
 from sys import platform as _platform
 
-#ActivityMonitor is located in ~/Library/Applications/Activity_Monitor.eze (I think)
+#ActivityMonitor is located in ~/Library/Applications/Utilities/Activity Monitor.app
 
 allowedUsers = ["root", "[adminName]", "_securityagent", "_appleevents", 
-                "_spotlight", "_usbmxd", "_dock", "_launchd"] #Users allowed and not to be logged by the SUC program
+                "_spotlight", "_usbmxd", "_dock", "_launchd", "_networkd", 
+                "_mdnsresponder", "_windowserver", "_softwareupdate", 
+                "_locationd", "_coreaudiod", "_netbios", "_cvmsroot"] #Users/SystemUsers/Daemons allowed and not to be logged by the SUC program
 
 def logNonUser():
   open('log.txt',  w) #will create a logFile if one does not already exist
+  
+  for theUser in allowedUsers:
+    if user != allowedUsers[theUser]:
+      write('log.txt', user)
 
 if _platform == "darwin": #Darwin = OSX
   logNonUser()
