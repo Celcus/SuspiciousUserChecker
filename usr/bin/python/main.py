4f55114a26552ce191380e020c3c87fc283ff72b
Celcus <arandomguy1337@gmail.com>
2015-12-08 10:58:58 -0600

    Update main.py

--- usr/bin/python/main.py
+++ usr/bin/python/main.py
@@ -3,6 +3,7 @@ from sys import platform as _platform
 
 #ActivityMonitor is located in ~/Library/Applications/Utilities/Activity Monitor.app
 
+updateFreq = 5 #can be edited depending on the time set on ActivtyMonitor's update frequency
 allowedUsers = ["root", "[adminName]", "_securityagent", "_appleevents", 
                 "_spotlight", "_usbmxd", "_dock", "_launchd", "_networkd", 
                 "_mdnsresponder", "_windowserver", "_softwareupdate", 
@@ -14,8 +15,11 @@ def logNonUser():
   for theUser in allowedUsers:
     if user != allowedUsers[theUser]:
       write('log.txt', user)
+  sleep(updateFreq) #Script waits X seconds before updating again
 
 if _platform == "darwin": #Darwin = OSX
   logNonUser()
 else:
   print "This program will probably not run for your OS..."
+  sleep(10) #Wait 10seconds before the program quits
+  sys.exit(1)
