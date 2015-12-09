fa9c0fe5b97ce5ba38cbdb23125229c67e47a6c6
Celcus <arandomguy1337@gmail.com>
2015-12-09 11:34:27 -0600

    Update main.py

--- usr/bin/python/main.py
+++ usr/bin/python/main.py
@@ -4,21 +4,30 @@ from sys import platform as _platform
 #ActivityMonitor is located in ~/Library/Applications/Utilities/Activity Monitor.app
 
 updateFreq = 5 #can be edited depending on the time set on ActivtyMonitor's update frequency
-allowedUsers = ["root", "[adminName]", "_securityagent", "_appleevents", 
+allowedUsersOSX = ["root", "[adminName]", "_securityagent", "_appleevents", 
                 "_spotlight", "_usbmxd", "_dock", "_launchd", "_networkd", 
-                "_mdnsresponder", "_windowserver", "_softwareupdate", 
-                "_locationd", "_coreaudiod", "_netbios", "_cvmsroot"] #Users/SystemUsers/Daemons allowed and not to be logged by the SUC program
+                "_mdnsresponder", "_windowserver", "_softwareupdate", "_locationd", 
+                "_coreaudiod", "_netbios", "_cvmsroot", "_netbios"] #Users/SystemUsers/Daemons allowed and ignored by the SUC script
 
-def logNonUser():
+allowedUsersLinux = []
+
+def logNonUserOSX():
   open('log.txt',  w) #will create a logFile if one does not already exist
   
-  for theUser in allowedUsers:
-    if user != allowedUsers[theUser]:
+  for theUser in allowedUsersOSX:
+    if user != allowedUsersOSX[theUser]:
       write('log.txt', user)
   sleep(updateFreq) #Script waits X seconds before updating again
+  
+def logNonUserLinux():
+  open('log.txt', w)
+  for theUser in allowedUsersLinux:
+    if user !- allowedUsersLinux[theUser]:
+      write('log.txt', user)
+    sleep(updateFreq)
 
 if _platform == "darwin": #Darwin = OSX
-  logNonUser()
+  logNonUserOSX()
 else:
   print "This program will probably not run for your OS..."
   sleep(10) #Wait 10seconds before the program quits
